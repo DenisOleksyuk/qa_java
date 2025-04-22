01a003ba1e30179e4024c62c1f201266eb5b6984
Denis Oleksyuk <InStanDI@yandex.ru>
2025-04-22 15:33:33 +0300

    выполнены задания 6го спринта

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -3,30 +3,29 @@ package com.example;
 import java.util.List;
 
 public class Lion {
+    private final boolean hasMane;
+    private final Predator predator;
 
-    boolean hasMane;
-
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Predator predator) throws Exception {
+        this.predator = predator;
         if ("Самец".equals(sex)) {
-            hasMane = true;
+            this.hasMane = true;
         } else if ("Самка".equals(sex)) {
-            hasMane = false;
+            this.hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
-    }
-
-    Feline feline = new Feline();
-
-    public int getKittens() {
-        return feline.getKittens();
     }
 
     public boolean doesHaveMane() {
         return hasMane;
     }
 
+    public int getKittens() {
+        return predator.getKittens();
+    }
+
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return predator.eatMeat();
     }
 }
